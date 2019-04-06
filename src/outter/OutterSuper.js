798ef57746816dd56a2f68c107da782bcc3908cf
Viacheslav Glazhevskyi <v.glazhevskyi@gmail.com>
2019-04-06 15:10:56 +0300

    fix issue when comment is blank

--- src/outter/OutterSuper.js
+++ src/outter/OutterSuper.js
@@ -4,6 +4,8 @@ export default class OutterSuper extends SheetWrapper {
   constructor(options, url) {
     super(options);
     this.url = url;
+    this.limit = 0;
+    this.start = 0;
   }
 
   get spreadsheet() {
@@ -18,24 +20,51 @@ export default class OutterSuper extends SheetWrapper {
     return this.memo.dataRange;
   }
 
+  limitTo(number) {
+    this.limit = number;
+    return this;
+  }
+
+  startFrom(number) {
+    this.start = number;
+    return this;
+  }
+
+  slice(coll) {
+    if (this.limit <= 0 || this.start < 0) return coll.slice(this.numHeaders);
+    if (this.limit < this.start) return coll.slice(this.numHeaders);
+    return coll
+      .slice(this.numHeaders)
+      .slice(this.start, this.limit);
+  }
+
   get values() {
-    return this.dataRange.getValues();
+    return this.slice(this.dataRange.getValues());
   }
 
   get formulas() {
-    return this.dataRange.getFormulas();
+    return this.slice(this.dataRange.getFormulas());
   }
 
   get notes() {
-    return this.dataRange.getNotes();
+    return this.slice(this.dataRange.getNotes());
   }
 
   extractAll() {
     const { values, formulas, notes } = this;
-    return values.map((rowValues, index) => ({
-      valuesObj: super.toRowData(rowValues),
-      formulasObj: super.toRowData(formulas[index]),
-      notesObj: super.toRowData(notes[index]),
-    })).slice(this.numHeaders); // remove header
+    const { length } = values;
+    const result = [];
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < length; i++) {
+      result.push({
+        // eslint-disable-next-line no-undef
+        uuid: Utilities.getUuid(),
+        // eslint-disable-next-line no-undef
+        valuesObj: super.toRowData(values[i]),
+        formulasObj: super.toRowData(formulas[i]),
+        notesObj: super.toRowData(notes[i]),
+      });
+    }
+    return result;
   }
 }
